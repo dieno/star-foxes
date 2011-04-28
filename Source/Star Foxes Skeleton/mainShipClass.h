941618e0987bc29632164c2d7cd9fb1ec983fdaf
boffley@gmail.com <boffley@gmail.com@2f9817a0-ed27-d5fb-3aa2-4a2bb642cc6a>
2011-04-28 21:46:12 +0000

    the Main Ship Class and player class skeletons

---
+++ Source/Star Foxes Skeleton/mainShipClass.h
@@ -0,0 +1,18 @@
+#ifndef MAINSHIPCLASS_H
+#define MAINSHIPCLASS_H
+
+class mainShipClass {
+public:
+	mainShipClass() {}
+	inline int getCurrentHealth() {
+		return currentHealth;
+	}
+	inline int getMaxHealth() {
+		return maxHealth;
+	}
+private:
+	int currentHealth;
+	int maxHealth;
+}
+
+#endif
