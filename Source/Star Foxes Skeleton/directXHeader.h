e18ae65ab35e9f7c4fb35883b54b443026e0669e
boffley@gmail.com <boffley@gmail.com@2f9817a0-ed27-d5fb-3aa2-4a2bb642cc6a>
2011-04-28 21:10:12 +0000

    original skeleton for direct X project

---
+++ Source/Star Foxes Skeleton/directXHeader.h
@@ -0,0 +1,10 @@
+#ifndef HDIRECTX
+#define HDIRECTX
+#include <d3d9.h>
+#include <d3dx9.h>
+#include <D3dx9tex.h>
+#include <windows.h>
+#include <tchar.h>
+#include <iostream>
+#include "directXClass.h"
+#endif
---
+++ Source/Star Foxes Skeleton/main.cpp
@@ -0,0 +1,12 @@
+#include "directXHeader.h"
+
+//main function, deals with everything
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pstrCmdLine, int iCmdShow) {
+	//filename of bitmap to display
+	wchar_t * bitmapName = TEXT("kohakhisui1204.bmp");
+	directXClass program = directXClass(bitmapName);
+	directXClass::program = &program;
+	program.WinMain(hInstance, hPrevInstance, pstrCmdLine, iCmdShow);
+}
+
+
