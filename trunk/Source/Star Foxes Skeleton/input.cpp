6df399cff14eeefa2c097abe31a95b380c27eed5
ryan.dieno@gmail.com <ryan.dieno@gmail.com@2f9817a0-ed27-d5fb-3aa2-4a2bb642cc6a>
2011-05-03 17:44:05 +0000

    added input classes (input.cpp / input.h)

---
+++ trunk/Source/Star Foxes Skeleton/input.cpp
@@ -0,0 +1,35 @@
+#include "Input.h"
+
+// sets up an interface to the keyboard
+void Input::init_keyboard(HWND hWnd)
+{
+	// creates a direct input object
+    DirectInput8Create(GetModuleHandle(NULL), 
+					   DIRECTINPUT_VERSION, 
+					   IID_IDirectInput8, 
+					   (void**)&keyb, 
+					   NULL);
+
+	// creates the keyboard device
+    keyb->CreateDevice(GUID_SysKeyboard, &keybdev, NULL);
+ 
+    keybdev->SetDataFormat(&c_dfDIKeyboard);
+    keybdev->SetCooperativeLevel(hWnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE);
+}
+
+// reads and responds to keyboard input
+void Input::read_keyboard(void)
+{
+	// get access if we don't have it already
+    keybdev->Acquire();
+
+    // get the input data
+    keybdev->GetDeviceState(256, (LPVOID)keystate);
+}
+
+// closes and cleans DirectInput
+void Input::clean_input(void)
+{
+	keybdev->Unacquire();    // make sure the keyboard is unacquired
+    keyb->Release();    // close DirectInput before exiting
+}
---
+++ trunk/Source/Star Foxes Skeleton/input.h
@@ -0,0 +1,23 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <dinput.h>
+
+class Input
+{
+private:
+	LPDIRECTINPUT8 keyb; // pointer to DirectInput interface
+	LPDIRECTINPUTDEVICE8 keybdev;    // the pointer to the keyboard device
+	BYTE keystate[256]; // stores keystroke information
+	
+public:
+	void init_keyboard(HWND hWnd); // sets up and inilializes the keyboard
+	void read_keyboard(void); // reads and responds to keyboard input
+	void clean_input(void); // cleans up direct input
+	
+	// returns true if a key has been pressed 
+	// eg. get_keystate(VK_UP) returns true if up has been pressed
+	inline BOOL get_keystate(int value){return (keystate[value] & 0x80);}
+};
+
+#endif
