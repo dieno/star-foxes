787193132e26828f4852afb0b710f5d263cf6b75
boffley@gmail.com <boffley@gmail.com@2f9817a0-ed27-d5fb-3aa2-4a2bb642cc6a>
2011-05-05 00:48:44 +0000

    added all methods from playerClass to mainPlayerClass.h and mainPlayerClass.cpp. added additional constructors to humanPlayerClass.h and mainPlayerClass.h. added mainPlayerClass.h to list of included files in directXHeader.h.

--- Source/Star Foxes Skeleton/mainPlayerClass.h
+++ Source/Star Foxes Skeleton/mainPlayerClass.h
@@ -10,8 +10,67 @@ public:
 		std::string playerName, 
 		int teamNum, 
 		int lives,
-		MainShipClass playerShip):playerName_(playerName), teamNum_(teamNum), lives_(lives), playerShip_(playerShip){}
+		MainShipClass playerShip):playerName_(playerName), teamNum_(teamNum), lives_(lives), playerShip_(playerShip),isAlive(true){}
+	MainPlayerClass():playerName_(),teamNum_(),lives_(),playerShip_(),isAlive(true){}
 
+	std::string getPlayerName() {
+		return playerName_;
+	}
+
+	int getTeamNum() {
+		return teamNum_;
+	}
+
+	int getLives() {
+		return lives_;
+	}
+
+	void drawSelf() {
+		playerShip_.renderSelf();
+	}
+
+	void bankLeft(float value);
+	void bankRight(float value);
+
+	void bankUp(float value)
+	{
+		vRotation.x += value;
+		playerShip_.setRotationAboutX(vRotation.x);
+	}
+
+	void bankDown(float value)
+	{
+		vRotation.x -= value;
+		playerShip_.setRotationAboutX(vRotation.x);
+	}
+
+	void useAfterBooster() {
+		afterboosterActive_ = true;
+		currentSpeed = 1000.0f; //place holder
+	}
+
+	void deactivateAfterBooster() {
+		afterboosterActive_ = false;
+		currentSpeed = 1.0f; //place holder
+	}
+
+	void updatePosition(); //put movement in here!
+
+	void updateRotation();
+
+	int getBoosterTimer() {
+		return boosterTimer;
+	}
+
+	int getShipCurrentHealth() {
+		return playerShip_.getCurrentHealth();
+	}
+
+	int getShipMaxHealth() {
+		return playerShip_.getMaxHealth();
+	}
+
+private:
 	//Player's  name (Bob, Bill, Bojangles, etc)
 	std::string playerName_;
 
@@ -23,6 +82,17 @@ public:
 
 	//The ship this player controls
 	MainShipClass playerShip_;
+
+	D3DXVECTOR3 vRotation;
+	D3DXVECTOR3 vPosition;
+	float rotationZ;
+	float rotationX;
+	float positionX;
+
+	int boosterTimer;
+	bool afterboosterActive_;
+	int currentSpeed;
+	bool isAlive;
 };
 
 
