171f2c7fa36a5bd91add39ce0a2b6b31c1f689d1
dannylau13@gmail.com <dannylau13@gmail.com@2f9817a0-ed27-d5fb-3aa2-4a2bb642cc6a>
2011-05-12 17:10:25 +0000

    Added #includes for the networking files

--- trunk/Source/Star Foxes Skeleton/directXHeader.h
+++ trunk/Source/Star Foxes Skeleton/directXHeader.h
@@ -1,14 +1,18 @@
 #ifndef HDIRECTX
 #define HDIRECTX
 
+#include <WinSock2.h>
 #include <d3d9.h>
 #include <d3dx9.h>
 #include <D3dx9tex.h>
 #include <dinput.h>
 #include <Windows.h>
 #include <list>
+#include <string>
+
 
 #pragma comment (lib, "dinput8.lib")
 #pragma comment (lib, "dxguid.lib")
+#pragma comment(lib,"ws2_32.lib")
 
 #endif
