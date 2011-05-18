4ec09cea6e95d504ab98e3b0a229ae13beccb451
enterintoexit@gmail.com <enterintoexit@gmail.com@2f9817a0-ed27-d5fb-3aa2-4a2bb642cc6a>
2011-05-18 18:05:40 +0000

    -Added gamestate skeleton.

---
+++ trunk/Source/Star Foxes Skeleton/gamestate.h
@@ -0,0 +1,28 @@
+#ifndef GAMESTATE_H
+#define GAMESTATE_H
+
+#include "directXHeader.h"
+#include "mainPlayerClass.h"
+
+//Holds on gamestate information relevant to AI - player locations, player health, special events (bombs), etc
+class GameState 
+{
+	private:
+		MainPlayerClass *players;
+		HWND hWnd_;
+
+	public:
+		GameState();
+		~GameState();
+
+	//Take in relevant game variables and update them within this object.
+	void updateGameState(HWND hWnd, MainPlayerClass *enemy);
+
+	//Returns player at specified index.
+	MainPlayerClass getPlayer(int idx);
+
+	//Returns hWnd
+	HWND getHWND();
+}
+
+#endif
