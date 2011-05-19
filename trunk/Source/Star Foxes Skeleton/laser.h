03e0d9fef24597e1ac3e68bf9d55281f2c8bb4f8
boffley@gmail.com <boffley@gmail.com@2f9817a0-ed27-d5fb-3aa2-4a2bb642cc6a>
2011-05-19 00:54:35 +0000

    added additional functionality to laser, it can now draw its self however it will always draw its self at (0, 0, 0) because setupWorld is not yet implemented.

--- trunk/Source/Star Foxes Skeleton/laser.h
+++ trunk/Source/Star Foxes Skeleton/laser.h
@@ -6,13 +6,16 @@
 
 class Laser {
 public:
-	Laser(int damage, D3DXVECTOR3 direction, D3DXVECTOR3 startPosition)
-		:damage(damage), speed(1), direction(direction), position(startPosition){}
+	Laser(int damage, D3DXVECTOR3 direction, D3DXVECTOR3 startPosition, MESHSTRUCT meshStruct, LPDIRECT3DDEVICE9 g_pDevice)
+		:damage(damage), speed(1), direction(direction), position(startPosition), meshStruct(meshStruct), g_pDevice(g_pDevice){}
 	int damage;
 	float speed;
 	D3DXVECTOR3 direction;
 	D3DXVECTOR3 position;
+	MESHSTRUCT meshStruct;
+	LPDIRECT3DDEVICE9 g_pDevice;
+	void drawSelf();
+	void setupWorld();
 };
-
 
 #endif
